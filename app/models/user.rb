ad6bf867d7cb32156b23702f949772dadfc4452a
Shabbir Ahmed <shabbir@salahsoftwaresolution.com>
2017-10-23 14:23:19 +0600

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
     has_many :posts, dependent: :destroy
+
+    mount_uploader :avatar, AvatarUploader
 end
